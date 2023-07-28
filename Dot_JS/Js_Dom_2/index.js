a05977333a8c0e9108a011423f5bba7d67fe3804
ashishinn <ashishinrewa@gmail.com>
2023-07-28 07:39:38 +0530

    dom class 2

---
+++ Dot_JS/Js_Dom_2/index.js
@@ -0,0 +1,42 @@
+// function eventFunction() {
+//   console.log("I have clicked on the document");
+// }
+
+// document.addEventListener("click", eventFunction);
+
+// document.removeEventListener("click", eventFunction);
+
+// const content = document.querySelector("#wrapper");
+
+// content.addEventListener("click", function (event) {
+//   console.log(event);
+// });
+
+// let links = document.querySelectorAll('a');
+// let thirdLink = links[2];
+
+// thirdLink.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     console.log('maxa aaya achha laga');
+// });
+
+// let myDiv = document.createElement("div");
+// function paraStatus(event) {
+//   console.log("Para " + event.target.textContent);
+// }
+// myDiv.addEventListener("click", paraStatus);
+// for (let i = 1; i < 100; i++) {
+//   let newElement = document.createElement("p");
+//   newElement.textContent = "This is para" + i;
+
+//   myDiv.appendChild(newElement);
+// }
+// document.body.appendChild(myDiv);
+
+let element = document.querySelector('#wrapper')
+element.addEventListener("click", function(event) {
+    if (event.target.nodeName === 'SPAN') {
+    console.log('span pr click kia hai' + event.target.textContent);
+    }
+});
+
